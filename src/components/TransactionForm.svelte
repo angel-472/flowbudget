8a543067997da873e40ac038a02faf02eee6345c
angel <108492882+angel-472@users.noreply.github.com>
2025-10-17 20:45:45 -0400

    fix: remove backdrop blur from TransactionForm modal

--- src/components/TransactionForm.svelte
+++ src/components/TransactionForm.svelte
@@ -83,8 +83,8 @@
   }
 </script>
 
-<div class="fixed inset-0 backdrop-blur-xs bg-black/40 flex items-center justify-center z-50  {isOpen ? '' : 'hidden'}">
-  <div class="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-2xl space-y-4 border-gray-100 dark:border-gray-700/50 border">
+<div class="fixed inset-0  bg-black/50 flex items-center justify-center z-50  {isOpen ? '' : 'hidden'}">
+  <div class="p-6 bg-white dark:bg-gray-800 rounded-xl space-y-4 shadow-2xl">
     <h3 class="text-xl font-bold text-gray-900 dark:text-white border-b border-gray-200 dark:border-gray-700 pb-2">
      {isNewTransaction ? 'Add' : 'Edit'} Transaction
     </h3>
