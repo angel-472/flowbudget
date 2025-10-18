0f0edc3d2c936f2f5b7a445a6f94f402589deae8
angel <108492882+angel-472@users.noreply.github.com>
2025-10-17 20:18:36 -0400

    Add backdrop blur to TransactionForm

--- src/components/TransactionForm.svelte
+++ src/components/TransactionForm.svelte
@@ -83,8 +83,8 @@
   }
 </script>
 
-<div class="fixed inset-0 bg-black/65 flex items-center justify-center z-50 {isOpen ? '' : 'hidden'}">
-  <div class="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-2xl space-y-4">
+<div class="fixed inset-0 backdrop-blur-xs bg-black/40 flex items-center justify-center z-50  {isOpen ? '' : 'hidden'}">
+  <div class="p-6 bg-white dark:bg-gray-800 rounded-xl shadow-2xl space-y-4 border-gray-100 dark:border-gray-700/50 border">
     <h3 class="text-xl font-bold text-gray-900 dark:text-white border-b border-gray-200 dark:border-gray-700 pb-2">
      {isNewTransaction ? 'Add' : 'Edit'} Transaction
     </h3>
